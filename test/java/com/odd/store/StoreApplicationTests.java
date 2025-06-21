df110afeab856134a9fa10a42d376717f99a98f7
AMIr <akbariamir079@gmail.com>
2025-06-21 12:54:45 +0330

    first point

---
+++ test/java/com/odd/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odd.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
